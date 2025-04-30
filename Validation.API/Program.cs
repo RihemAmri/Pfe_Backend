3249efd4b3fb9c27af4a22003a8caef89953717c
RihemAmri <amririhem21@gmail.com>
2025-04-30 13:08:03 +0100

    add dinktopdlib

--- Validation.API/Program.cs
+++ Validation.API/Program.cs
@@ -2,8 +2,6 @@ using DinkToPdf;
 using DinkToPdf.Contracts;
 using Validation.API.Services;
 using Validation.API.Repositories;
-using System.Reflection;
-using System.Runtime.Loader;
 
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
@@ -11,23 +9,20 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
     Args = args
 });
 
+// ✅ PAS besoin de charger manuellement la DLL avec CustomAssemblyLoadContext
 
-// Charger la DLL native wkhtmltox
-var wkHtmlToPdfPath = Path.Combine(Directory.GetCurrentDirectory(), "DinkToPdfLib", "64 bit", "libwkhtmltox.dll");
-var context = new CustomAssemblyLoadContext();
-context.LoadUnmanagedLibrary(wkHtmlToPdfPath);
-//builder.WebHost.UseWebRoot("wwwroot");
-// Add services to the container
+builder.WebHost.UseUrls("http://*:4010");
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
-        });
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -37,43 +32,19 @@ builder.Services.AddScoped<IValidationService, ValidationService>();
 builder.Services.AddScoped<IValidationRepository, ValidationRepository>();
 builder.Services.AddScoped<PdfService>();
 builder.Services.AddScoped<EmailService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseStaticFiles();
 
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
